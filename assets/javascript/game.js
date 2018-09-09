2ede6611f40a95283242ce844c2d6fcd443845e6
Eduardo Monforte <eduardomonforte@gmail.com>
2018-09-08 21:41:50 -0500

    Added some basic functions.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,36 @@
+// Just so I know the file is linked!
+
 console.log("It's working!")
+
+//Pseudo-Code
+
+// Scores
+
+// Win Score starts at 0.
+var winScore = 0;
+document.getElementById("win-score").innerHTML = winScore
+
+// Loss Score starts at 0.
+var lossScore = 0;
+document.getElementById("loss-score").innerHTML = lossScore
+
+// Guesses starts at 13.
+var guesses = 13;
+document.getElementById("guesses-left").innerHTML = guesses
+
+// 1. The computer randomnly chooses one of the letters of the alphabet.
+
+// This array contains all the letters in the alphabet, each one as a string.
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+console.log(letters)
+
+// This function randomly chooses a letter inside the letters array.
+
+var chosenLetter = letters[Math.floor(Math.random() * letters.length)];
+console.log(chosenLetter)
+
+
+function myFunction() {
+    var x = document.getElementById("fname");
+    x.value = x.value.toUpperCase();
+}
